7544e3d902a2acaebe4bb4905e2142727e68edcd
andreasteel <102710199+andreasteel@users.noreply.github.com>
2023-02-22 14:14:33 -0700

    models etc

---
+++ Mission8_Project/Models/Quadrant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission8_Project.Models
+{
+    public class Quadrant
+    {
+        [Key]
+        [Required]
+        public int QuadrantId { get; set; }
+        public string Quadrants { get; set; }
+    }
+}
---
+++ Mission8_Project/Models/TaskContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission8_Project.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext (DbContextOptions<TaskContext> options) : base(options)
+        {
+        //blank
+        }
+
+        public DbSet<TaskForm> Responses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Quadrant> Quadrants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category { Categories = "Home" },
+                new Category { Categories = "School" },
+                new Category { Categories = "Work" },
+                new Category { Categories = "Church" }
+                );
+            mb.Entity<Quadrant>().HasData(
+                new Quadrant { QuadrantId = 1, Quadrants = "Quadrant 1" },
+                new Quadrant { QuadrantId = 2, Quadrants = "Quadrant 2" },
+                new Quadrant { QuadrantId = 3, Quadrants = "Quadrant 3" },
+                new Quadrant { QuadrantId = 4, Quadrants = "Quadrant 4" }
+                );
+        }
+    }
+}
