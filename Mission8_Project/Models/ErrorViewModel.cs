dbb28bd3145c15d53808105b78b650f16ac939a3
Josh Blad <jpb82@byu.edu>
2023-02-22 13:21:45 -0700

    Add project files.

---
+++ Mission8_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission8_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
